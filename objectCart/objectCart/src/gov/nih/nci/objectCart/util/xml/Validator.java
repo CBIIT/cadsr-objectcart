47cda95650c9e486baa14c52086413f00a00889f
avdicd <avdicd>
2008-03-26 19:51:32 +0000

    *** empty log message ***
    SVN-Revision: 5

--- objectCart/objectCart/src/gov/nih/nci/objectCart/util/xml/Validator.java
+++ objectCart/objectCart/src/gov/nih/nci/objectCart/util/xml/Validator.java
@@ -32,44 +32,48 @@ public class Validator {
 	
 	public static void validateObject(CartObject cartObject)
 			throws ApplicationException {
-		try {
-			SAXParserFactory factory = SAXParserFactory.newInstance();
-			factory.setValidating(false);
-			factory.setNamespaceAware(true);
-
-			SchemaFactory schemaFactory = SchemaFactory
-					.newInstance("http://www.w3.org/2001/XMLSchema");
-			
-			String schema = getSchema(cartObject.getType());
-			
-			factory.setSchema(
-					schemaFactory.newSchema(
-							new Source[] {new StreamSource(schema)} ));
-
-			SAXParser parser = factory.newSAXParser();
-
-			XMLReader reader = parser.getXMLReader();
-
-			reader.setErrorHandler(new ValidationErrorHandler());
-			StringReader strReader = new StringReader(cartObject.getData());
-			InputSource inputSource = new InputSource(strReader);
-
-			reader.parse(inputSource);
-
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-			log.error("Parser Configuration Error");
-			throw new ApplicationException("Parser Configuration Error", e);
-		} catch (SAXException e) {
-			e.printStackTrace();
-			log.error("Error during validation");
-			throw new ApplicationException("Error During Validation", e);
-        } catch (IOException e) {
-            e.printStackTrace();
-            log.error("IOException in Validation");
-            throw new ApplicationException("IO Exception in Validation", e);
-        }	
 		
+		if (!cartObject.getType().startsWith(":Serialized:") &&
+			!cartObject.getType().startsWith(":Test:"))
+		{
+			try {
+				SAXParserFactory factory = SAXParserFactory.newInstance();
+				factory.setValidating(false);
+				factory.setNamespaceAware(true);
+	
+				SchemaFactory schemaFactory = SchemaFactory
+						.newInstance("http://www.w3.org/2001/XMLSchema");
+				
+				String schema = getSchema(cartObject.getType());
+				
+				factory.setSchema(
+						schemaFactory.newSchema(
+								new Source[] {new StreamSource(schema)} ));
+	
+				SAXParser parser = factory.newSAXParser();
+	
+				XMLReader reader = parser.getXMLReader();
+	
+				reader.setErrorHandler(new ValidationErrorHandler());
+				StringReader strReader = new StringReader(cartObject.getData());
+				InputSource inputSource = new InputSource(strReader);
+	
+				reader.parse(inputSource);
+	
+			} catch (ParserConfigurationException e) {
+				e.printStackTrace();
+				log.error("Parser Configuration Error");
+				throw new ApplicationException("Parser Configuration Error", e);
+			} catch (SAXException e) {
+				e.printStackTrace();
+				log.error("Error during validation");
+				throw new ApplicationException("Error During Validation", e);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            log.error("IOException in Validation");
+	            throw new ApplicationException("IO Exception in Validation", e);
+	        }	
+		}
 	}
 	
 	
@@ -79,11 +83,14 @@ public class Validator {
 		
 		try { 
 			Namespace ns = new Namespace(name);
+			File f = new File("localcontent/"+name);
+		
 			XMLDataModelService handle = 
 				(XMLDataModelService) GridServiceResolver.getInstance().getGridService("http://dc04.bmi.ohio-state.edu:8080/ogsa/services/cagrid/gme"); 
-			SchemaNode node = handle.getSchema(ns,true);
+			List nodes = handle.cacheSchema(ns, f);
 			
-			return node.getSchemaContents();
+			String schema = constructSchema(nodes);
+			return schema;
 			
 			
 		} catch (MobiusException e1) {
@@ -92,6 +99,15 @@ public class Validator {
 		return null;
 	}
 		
+	private static String constructSchema(List nodes) {
+		
+		for (Object o: nodes){
+			SchemaNode sn = (SchemaNode) o;
+			System.out.println("+++++++++++++++++++++");
+			System.out.println(sn.getSchemaContents());
+		}
+		return "";
+	}
 }
 
 
